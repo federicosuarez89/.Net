b34f95aff7236efd22b4334e2e0e349c1343a3fa
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-15 14:19:40 -0300

    Hola Mundo en .Net (Lucas Moy)

---
+++ Programador .Net/Programador .Net/Form1.cs
@@ -0,0 +1,15 @@
+namespace Programador_.Net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
