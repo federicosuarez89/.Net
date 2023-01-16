c832b0cd6558bad9570b42fb4cfc42b5ceef551f
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-16 10:44:14 -0300

    Variables,nomenclatura (video 4)

--- PildorasInformaticas/PrimeraAplicacion/Program.cs
+++ PildorasInformaticas/PrimeraAplicacion/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
+            //Comentario de una sola linea
             Console.WriteLine("Hola desde c#");
+
+            /*
+                Comentario
+                de varias 
+                lineas
+            */
+            int edad = 28;
+            Console.WriteLine(edad);
         }
     }
 }
