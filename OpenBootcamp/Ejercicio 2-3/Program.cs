e798f2e1ea7bcfc71dfdad94fbf1a66b32ed7353
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-18 10:12:19 -0300

    Ejercicios sesion 4(OpenBootcamp)

--- OpenBootcamp/Ejercicio 2-3/Program.cs
+++ OpenBootcamp/Ejercicio 2-3/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("Ejercicio 2");
+            //Coche
+            int puertas, ruedas;
+            string marca;
+            bool ITV;
+            //Mesa
+            double peso, largo;
+            string material, color;
+
+            Console.WriteLine("Ejercicio 3");
+            
+            //Comprobamos si un numero es igual o mayor que 18
+            int numero2 = int.Parse(Console.ReadLine());
+            if (numero2 >=18) {
+                Console.WriteLine("El numero es mayor o igual que 18");
+            }
+            else
+            {
+                Console.WriteLine("El numero es menor de 18");
+            }
+
+            //Comprobamos si un char es igual a 'a' 
+            Console.WriteLine("Ingrese un caracter");
+            char caracter = char.Parse(Console.ReadLine());
+            if(caracter== 'a')
+            {
+                Console.WriteLine("Es igual");
+            }
+            else
+            {
+                Console.WriteLine("Es distinto");
+            }
+
+            //Comprobamos 2 condiciones a la vez
+            Console.WriteLine("Ingrese el primer valor");
+            int condicion1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo valor");
+            int condicion2 = int.Parse(Console.ReadLine());
+            if(condicion1>=18 && condicion2>=18)
+            {
+                Console.WriteLine("Ambas personas son mayores de edad");
+            }else if(condicion1>=18 && condicion2 <= 18)
+            {
+                Console.WriteLine("La primera persona es mayor de edad y la segunda es menor");
+            }else if(condicion1<=18 && condicion2 >= 18)
+            {
+                Console.WriteLine("La segunda persona es mayor de edad y la primera es menor");
+            }
+            else
+            {
+                Console.WriteLine("Ambas personas son menores de edad");
+            }
+
+
+
+
+}
     }
 }
---
+++ OpenBootcamp/EjerciciosSesion4/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace EjerciciosSesion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Ejercicio 1 - While.Escribe una tabla de multiplicar del 1 al 10 
+                para un número entero que recibe por consola.
+            */
+            Console.WriteLine("Ingrese un numero para calcular la tabla de multiplicar");
+            int numUsuario = int.Parse(Console.ReadLine());
+            int contador = 0;
+            
+            while(contador < 1)
+            {
+                Console.WriteLine($"Tabla del {numUsuario}");
+                tablaMultiplicar(numUsuario);
+                contador++;
+            }
+
+            /*
+                Ejercicio 2 - Do while
+
+                Escribe un programa que realice estos pasos:
+
+                Reciba al menos un número por consola
+
+                Determine si el número es positivo o negativo
+
+                Presente un contador para cada tipo (positivo y negativo).
+             */
+            Console.WriteLine("¿Cuantos numeros se van a evaluar");
+            int cantNumeros = int.Parse(Console.ReadLine());
+            int numeroIngresado;
+            int contador2 = 0;
+            int contadorNegativos = 0;
+            int contadorPositivos = 0;
+            int contadorCeros = 0;
+            int otroContador = 1;
+            int ottroContador2 = 0;
+            do
+            {
+                Console.WriteLine($"{ otroContador}.Ingrese un numero");
+                numeroIngresado = int.Parse(Console.ReadLine());
+                if (numeroIngresado < 0)
+                {
+                    contadorNegativos++;
+                }
+                else if (numeroIngresado > 0)
+                {
+                    contadorPositivos++;
+                }
+                else if (numeroIngresado == 0)
+                {
+                    contadorCeros++;
+                }
+                otroContador++;
+                ottroContador2++;
+            } while (ottroContador2<cantNumeros);
+            Console.WriteLine($"Cantidad de numeros negativos: {contadorNegativos}");
+            Console.WriteLine($"Cantidad de numeros positivos: {contadorPositivos}");
+            Console.WriteLine($"Cantidad de numeros cero: {contadorCeros}");
+        }
+
+        //Metodo para las tablas de multiplicar
+        static int tablaMultiplicar(int multiplicador)
+        {
+            int resultado = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                resultado = multiplicador * i;
+                Console.WriteLine(multiplicador+"*"+i+"="+resultado);
+            }
+            return resultado;
+        }
+    }
+}
