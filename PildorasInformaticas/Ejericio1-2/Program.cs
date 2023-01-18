d22ac78a2d268a850a157a2aa18f0b5fb9272edd
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-17 23:22:13 -0300

    Ejercicio 1-2 (OpenBootcamp)

---
+++ PildorasInformaticas/Ejericio1-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejericio1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es: " + nombre);
+        }
+    }
+}
