5fcfc98f306909872a40c288b6c0f1a9c2443b49
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-19 08:26:37 -0300

    Creacion de clases e instancias (video 28)

---
+++ PildorasInformaticas/POO/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciamos un objeto de tipo Circulo
+            Circulo miCirculo = new Circulo();
+            Console.WriteLine("El valor del area es: " + miCirculo.area(5));
+
+            Circulo miCirculo2 = new Circulo();
+            Console.WriteLine("El valor del area es: " + miCirculo2.area(10));
+            
+        }
+    }
+
+    //Creamos una nueva clase
+    class Circulo
+    {
+        //Atributos de clase
+        double pi = 3.1416;
+
+        //Metodos de clase
+        public double area(int radio)
+        {
+            return pi * radio * radio;
+        }
+
+    }
+}
