2b4a150ebed760252fcde0b4d1f0961763e2da1c
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-20 11:30:04 -0300

    Encapsulamiento (video 29)

--- PildorasInformaticas/POO/Program.cs
+++ PildorasInformaticas/POO/Program.cs
@@ -6,11 +6,18 @@
         {
             //Instanciamos un objeto de tipo Circulo
             Circulo miCirculo = new Circulo();
-            Console.WriteLine("El valor del area es: " + miCirculo.area(5));
+            Console.WriteLine("El valor del area es: " + miCirculo.CalculoArea(5));
 
             Circulo miCirculo2 = new Circulo();
-            Console.WriteLine("El valor del area es: " + miCirculo2.area(10));
-            
+            Console.WriteLine("El valor del area es: " + miCirculo2.CalculoArea(10));
+
+            //Instanciamos un objeto de tipo ConversorPesoDolar
+            ConversorPesoDolar objeto = new ConversorPesoDolar();
+            Console.WriteLine(objeto.ConversorDivisa(100));
+
+            //Cambiamos el valor del dolar
+            objeto.cambiarDolar(-360);
+            Console.WriteLine(objeto.ConversorDivisa(100));
         }
     }
 
@@ -18,13 +25,39 @@
     class Circulo
     {
         //Atributos de clase
-        double pi = 3.1416;
+        private const double pi = 3.1416;
 
         //Metodos de clase
-        public double area(int radio)
+        public double CalculoArea(int radio)
         {
             return pi * radio * radio;
         }
 
+    }
+
+    //Creamos otra clase 
+    class ConversorPesoDolar
+    {
+        private double dolar = 370;
+
+        //Metodo para convertir Dolar a Peso
+        public double ConversorDivisa(double cantidadDolares)
+        {
+            return cantidadDolares * dolar;
+        }
+
+        //Metodo para modificar el valor del dolar en Argentina
+        public void cambiarDolar(double valorDolar)
+        {
+            if (valorDolar < 0)
+            {
+                dolar = dolar;
+            }
+            else
+            {
+                dolar = valorDolar;
+            }
+            
+        }
     }
 }
